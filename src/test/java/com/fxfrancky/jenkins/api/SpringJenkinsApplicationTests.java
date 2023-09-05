81101fee69fa5f9abe359e4d67f304a0439bf26a
Franck Owen <fxfrancky@yahoo.com>
2023-09-05 21:10:12 +0200

    second commit

--- src/test/java/com/fxfrancky/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/fxfrancky/jenkins/api/SpringJenkinsApplicationTests.java
@@ -17,6 +17,7 @@ class SpringJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing...");
+		logger.info("Test case executing second log statement...");
 		assertEquals(Boolean.TRUE,true);
 	}
 
