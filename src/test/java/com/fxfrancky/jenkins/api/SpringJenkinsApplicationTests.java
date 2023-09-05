e80d3e64af90d36197ef10a47d4530e312d2b735
Franck Owen <fxfrancky@yahoo.com>
2023-09-05 20:13:56 +0200

    first commit

---
+++ src/test/java/com/fxfrancky/jenkins/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,23 @@
+package com.fxfrancky.jenkins.api;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.AbstractContextLoader;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test case executing...");
+		assertEquals(Boolean.TRUE,true);
+	}
+
+}
